4a481762d77d5cbf6dc3e94d1d4575c01cc7a0cc
Stéphane <stephanethomarat92@hotmail.com>
2019-10-01 15:25:15 +0200

    Merge branch 'master' of https://github.com/makhout971/Projet-Sortir
     Conflicts:
    	templates/user/login.html.twig

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserController extends Controller
 {
