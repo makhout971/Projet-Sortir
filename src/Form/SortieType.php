4369fd3a0d8bc0fc85b184ca52635c2c51e1f598
Stéphane <stephanethomarat92@hotmail.com>
2019-10-03 11:16:18 +0200

    Merge branch 'master' of https://github.com/makhout971/Projet-Sortir
     Conflicts:
    	templates/user/login.html.twig

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -34,6 +34,7 @@ class SortieType extends AbstractType
             ->add('dateHeureDebut', DateType::class, [
                 'label' => 'Date et heure du début : ',
                 'required' => true,
+                'sfValidatorDate ' => date_timestamp_get()
 
             ])
 
