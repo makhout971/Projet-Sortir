d549ced3aae6aee156fac1737a0abe11b9a895e9
Stéphane <stephanethomarat92@hotmail.com>
2019-10-03 17:15:42 +0200

    Merge branch 'master' of https://github.com/makhout971/Projet-Sortir
     Conflicts:
    	templates/user/login.html.twig

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Lieu;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,25 @@ class LieuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('rue')
+            ->add('nomLieu', TextType::class,[
+                'label' => 'Nom: ',
+                'required' => true,
+            ])
+            ->add('rue', TextType::class,[
+                'label' => 'Rue: ',
+                'required' => true,
+            ])
+//            ->add('ville', TextType::class,[
+//                'label' => 'Ville: ',
+//                'required' => true,
+//            ])
+//            ->add('code', IntegerType::class,[
+//                'label' => 'Code postal: ',
+//                'required' => true,
+//            ])
+            ->add('ville', VilleType::class,[
 
+            ])
         ;
     }
 
